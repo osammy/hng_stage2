f966fd791633c83a31291ebe7e7ae947276d31a8
osammy <osamaimafidon@gmail.com>
2018-04-21 00:27:27 +0100

    Add files via upload

---
+++ stage1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<!Doctype html>
+<html>
+<head></head>
+<body>
+<div style="background-color:#c4c4c4;text-align:center;height:331px;width:758px;margin-left:100px">
+<div style="padding-top:100px;font-size:1.2em">
+<?php
+date_default_timezone_set("Africa/Lagos");
+
+echo "The current time is " ."<br>" . date("h:i:sa");
+?>
+</div>
+</div>
+</body>
+</html
